6cfcb73ffe64178ae34d8f46a38f6fb3a52e51df
marinaborisyuk <marinaborisyuk1@gmail.com>
2023-11-10 13:37:09 +0300

    feat: add google tag

--- src/components/Layout/MainLayout/Layout.tsx
+++ src/components/Layout/MainLayout/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '@/components/UI/Header';
 import * as styles from './styles';
+import Script from 'next/script';
 
 interface ILayoutProps {
   title?: string;
@@ -10,6 +11,15 @@ interface ILayoutProps {
 export default function Layout({ title, children }: ILayoutProps) {
   return (
     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       <Head>
         <title>{title ? title : 'Shoes Shop'}</title>
         <meta name="description" content="Shoes Shop. The best solution for your comfort" />
