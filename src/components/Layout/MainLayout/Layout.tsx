acbbf215ca6cd38a05edecad292386c068c2dad6
marinaborisyuk <marinaborisyuk1@gmail.com>
2023-11-24 21:46:07 +0300

    fix: update tmg script

--- src/components/Layout/MainLayout/Layout.tsx
+++ src/components/Layout/MainLayout/Layout.tsx
@@ -11,8 +11,17 @@ interface ILayoutProps {
 export default function Layout({ title, children }: ILayoutProps) {
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`} />
+      {/* <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`} /> */}
       <Head>
+        <script>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', ${process.env.NEXT_PUBLIC_GTM_ID});
+        `}
+        </script>
         <title>{title ? title : 'Shoes Shop'}</title>
         <meta name="description" content="Shoes Shop. The best solution for your comfort" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
