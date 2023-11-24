100700e2ef27a9655b65c5a1183afcd4d27796f9
marinaborisyuk <marinaborisyuk1@gmail.com>
2023-11-24 21:55:52 +0300

    revert to previous version

--- src/components/Layout/MainLayout/Layout.tsx
+++ src/components/Layout/MainLayout/Layout.tsx
@@ -11,9 +11,8 @@ interface ILayoutProps {
 export default function Layout({ title, children }: ILayoutProps) {
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id='${process.env.NEXT_PUBLIC_GTM_ID}'`} />
       <Head>
-        {/* <script>
+        <script>
           {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -21,7 +20,7 @@ export default function Layout({ title, children }: ILayoutProps) {
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
         `}
-        </script> */}
+        </script>
         <title>{title ? title : 'Shoes Shop'}</title>
         <meta name="description" content="Shoes Shop. The best solution for your comfort" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
